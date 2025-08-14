2a6ed1637164b9a23a74c7d53191f9d476c9d122
Godwin Igbokwe <godwinigbokwe@Godwins-MacBook-Pro.local>
2025-08-14 19:28:22 +0100

    update: tweak app.dart for Budget Planner shell

--- lib/app.dart
+++ lib/app.dart
@@ -1,2 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:budget_planner/core/routing/app_router.dart';
+
+class BudgetApp extends StatelessWidget {
+  const BudgetApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //use material router for now
+  }
+}
